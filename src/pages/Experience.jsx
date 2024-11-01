48d78d9134c1ec5115575f3a2bfaf9e15bb59a51
unknown <falihfauzan64@gmail.com>
2024-11-01 22:24:19 +0700

    update responsive 2

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import { HiMiniBuildingOffice } from "react-icons/hi2";
+import "../style/Experience.css";
 
 function Experience() {
   return (
@@ -15,76 +16,78 @@ function Experience() {
       <Navbar />
       <div className="wrapper">
         <h1>Experience</h1>
-        <VerticalTimeline>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="November 2024 - Present"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<FaLaravel />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              Programmer IT Developer (Internship)
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              FYP Media <HiMiniBuildingOffice />
-            </h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, SEO, Online
-              Marketing
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="July 2024 - November 2024"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<FaLaravel />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              IT Full Stack (Internship)
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Soko Financial <HiMiniBuildingOffice />
-            </h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, SEO, Online
-              Marketing
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="February 2024 - June 2024 "
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<FaLaravel />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              Program Studi Independen Bersertifikat (SIB) Kampus Merdeka
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              PT Educa Sisfomedia Indonesia (Gamelab) <HiMiniBuildingOffice />
-            </h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, SEO, Online
-              Marketing
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="March 2019 - June 2019"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<FaLaravel />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              IT Staff (Internship)
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              PT LUA Kreatif Teknologi <HiMiniBuildingOffice />
-            </h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, SEO, Online
-              Marketing
-            </p>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
+        <div className="timeline-experience">
+          <VerticalTimeline>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date="November 2024 - Present"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<FaLaravel />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                Programmer IT Developer (Internship)
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                FYP Media <HiMiniBuildingOffice />
+              </h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, SEO, Online
+                Marketing
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date="July 2024 - November 2024"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<FaLaravel />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                IT Full Stack (Internship)
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                Soko Financial <HiMiniBuildingOffice />
+              </h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, SEO, Online
+                Marketing
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date="February 2024 - June 2024 "
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<FaLaravel />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                Program Studi Independen Bersertifikat (SIB) Kampus Merdeka
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                PT Educa Sisfomedia Indonesia (Gamelab) <HiMiniBuildingOffice />
+              </h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, SEO, Online
+                Marketing
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date="March 2019 - June 2019"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<FaLaravel />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                IT Staff (Internship)
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                PT LUA Kreatif Teknologi <HiMiniBuildingOffice />
+              </h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, SEO, Online
+                Marketing
+              </p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
       </div>
       <Footer />
       <ScrollToTop />
